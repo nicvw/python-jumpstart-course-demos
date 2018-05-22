19d7bc2d6e48200542c5e77a4d2b439df7085745
Nic von Waltsleben <nicholas.vonwaltsleben@voss-solutions.com>
2018-05-22 08:10:08 +0200

    yawn...

---
+++ apps/01_hello_world/you_try/program.py
@@ -0,0 +1,12 @@
+def main():
+    print('----------------------------')
+    print('       HELLO APP')
+    print('----------------------------')
+    print()
+
+    user_input = input('What is your name? ')
+    print("Nice to meet you {}".format(user_input))
+
+
+if __name__ == '__main__':
+    main()
